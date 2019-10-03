39ea763f0127d401a58f9818f9464515b91ece8f
150536371@qq.com <1505363731@qq.com>
2019-10-03 17:42:17 +0800

    。。。。。。。。。

--- src/main/java/com/qfedu/service/impl/UserServiceImpl.java
+++ src/main/java/com/qfedu/service/impl/UserServiceImpl.java
@@ -1,47 +1,31 @@
 package com.qfedu.service.impl;
 
-
-
-
 import com.qfedu.entry.User;
 import com.qfedu.mapper.UserMapper;
 import com.qfedu.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class UserServiceImpl implements UserService {
-<<<<<<< HEAD
 
-
-=======
->>>>>>> origin/master
     @Autowired
-    UserMapper mapper;
+    UserMapper userMapper;
+
 
     @Override
-<<<<<<< HEAD
     public boolean addUser(User user) {
-        int rows = mapper.addUser(user);
+
+        int rows = userMapper.addUser(user);
 
         System.out.println("userId"+rows);
 
         return rows > 0 ? true : false ;
-
     }
 
     @Override
     public User selectByUserName(String userName) {
-        return mapper.selectByUserName(userName);
-
-
-
-
-
-=======
-    public User selectByUserName(String userName) {
-        return mapper.selectByUserName(userName);
->>>>>>> origin/master
+        return null;
     }
-
 }
