8bc3a5acfa0006ab6a19c957abad71a11d657e53
dingqiyu <1195261106@qq.com>
2019-10-04 17:45:53 +0800

    关注度实体类

---
+++ src/main/java/com/qfedu/entry/Attention.java
@@ -0,0 +1,16 @@
+package com.qfedu.entry;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+
+@Data
+public class Attention {
+    private Integer id;
+
+    private Integer username;
+
+    private Integer bid;
+
+
+}
